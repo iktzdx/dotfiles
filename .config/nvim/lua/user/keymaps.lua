a66edbdacfef964f9ade36eaef7f87430ed4b2dd
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2023-10-07 21:23:36 +0300

    add keymaps to manage tabs

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -16,6 +16,11 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- open the current split into its own tab (for dap-ui)
+vim.keymap.set("n", "<leader>tn", ":tabnew %<CR>")
+-- close the current tab
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>")
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
