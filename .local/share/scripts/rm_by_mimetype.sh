c109d489cc14a7a4d17139e322a0256337683199
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-01-28 00:16:38 +0300

    Refactor rm_by_mimetype.sh script: use awk and regex instead of just grep

--- .local/share/scripts/rm_by_mimetype.sh
+++ .local/share/scripts/rm_by_mimetype.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-MIME_TYPES=(image video zip pdf)
+declare -A MIME_TYPES=(
+    ["image"]="^image/"
+    ["video"]="^video/"
+    ["archive"]="^(application\/x-tar|application\/x-7z-compressed|application\/x-xz|application\/x-rar-compressed|application\/zip|application\/gzip|application\/x-bzip2)"
+    ["pdf"]="application/pdf"
+)
 
 PS3="Choose MIME-type: "
-select type in "${MIME_TYPES[@]}"; do
-    find . -maxdepth 1 -type f -exec file --mime-type {} + \
-        | grep "$type" | cut -d: -f1 | tr '\n' '\0' \
-        | xargs -0 rm -v;
+select type in "${!MIME_TYPES[@]}"; do
+    regex=${MIME_TYPES[$type]}
+    find . -maxdepth 1 -type f -exec file --mime-type {} \; \
+        | awk -v regex="$regex" -F": " '$2 ~ regex { print $1 }' | tr '\n' '\0' \
+        | xargs -0 rm -v
     break
 done
