bc74eec4ffdc45142cd9f63fe8ff0b0baa321990
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-01-27 23:52:59 +0300

    Refactor rm_by_mimetype.sh script: use select instead of fzf

--- .local/share/scripts/rm_by_mimetype.sh
+++ .local/share/scripts/rm_by_mimetype.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-types=$(echo "image video pdf zip" | tr ' ' '\n')
+MIME_TYPES=(image video zip pdf)
 
-selected=$(echo "$types" | fzf)
-
-find . -maxdepth 1 -type f -exec file --mime-type {} + \
-    | grep "$selected" | cut -d: -f1 | tr '\n' '\0' \
-    | xargs -0 rm -v
+PS3="Choose MIME-type: "
+select type in "${MIME_TYPES[@]}"; do
+    find . -maxdepth 1 -type f -exec file --mime-type {} + \
+        | grep "$type" | cut -d: -f1 | tr '\n' '\0' \
+        | xargs -0 rm -v;
+    break
+done
